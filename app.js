2835c7bda0229c6dbb372752fb9c080fee6965ef
unknown <sivakaresh@gmail.com>
2019-09-19 20:41:36 +0530

    final touch good -> better css/js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const socreBoard_div = document.querySelector(".score-board");
 const result_div = document.querySelector(".result > p");
-const rock_div = document.getElementById("rock");
-const paper_div = document.getElementById("paper");
-const scissor_div = document.getElementById("scissor");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
 
 function getComputerChoice() {
 	const choices = ['r','p','s'];
@@ -28,6 +28,9 @@ function win(userChoice,computerChoice){
 	const smallUserWord = "user".fontsize(3).sub();
 	const smallCompWord = "comp".fontsize(3).sub();
 	result_div.innerHTML=`${convertToWord(userChoice)}${smallUserWord} beats ${convertToWord(computerChoice)}${smallCompWord} . You Win !!! &#9924`;
+	document.getElementById(userChoice).classList.add('green-glow');
+	// setTimeout(function(){document.getElementById(userChoice).classList.remove('green-glow')}, 300);
+	setTimeout(() => document.getElementById(userChoice).classList.remove('green-glow'), 300);
 }
 function lose(userChoice,computerChoice){
 	computerScore++;
@@ -36,11 +39,15 @@ function lose(userChoice,computerChoice){
 	const smallUserWord = "user".fontsize(3).sub();
 	const smallCompWord = "comp".fontsize(3).sub();
 	result_div.innerHTML=`${convertToWord(userChoice)}${smallUserWord} loses to ${convertToWord(computerChoice)}${smallCompWord} . You Lost... &#9940`;
+	document.getElementById(userChoice).classList.add('red-glow');
+	setTimeout(function(){document.getElementById(userChoice).classList.remove('red-glow')}, 300);
 }
 function draw(userChoice,computerChoice){
 	const smallUserWord = "user".fontsize(3).sub();
 	const smallCompWord = "comp".fontsize(3).sub();
 	result_div.innerHTML=`${convertToWord(userChoice)}${smallUserWord} equals ${convertToWord(computerChoice)}${smallCompWord} . It's a draw. &#9889`;
+	document.getElementById(userChoice).classList.add('gray-glow');
+	setTimeout(function(){document.getElementById(userChoice).classList.remove('gray-glow')}, 300);
 }
 // console.log(getComputerChoice());
 function game(userChoice){
@@ -73,9 +80,10 @@ rock_div.addEventListener('click',function(){
 	game("r");
 })
 
-paper_div.addEventListener('click',function(){
-	game("p");
-})
+// paper_div.addEventListener('click',function(){
+// 	game("p");
+// })
+paper_div.addEventListener('click',() => game("p"));
 
 scissor_div.addEventListener('click',function(){
 	game("s");
